0077e4d75844af69cb205b3d951a65b5de4db45c
Thomas Harrop <thomas.harrop@csiro.au>
2024-10-23 21:18:38 +1100

    run a lot of useful tests

--- submit_snakemake_to_sbatch.sh
+++ submit_snakemake_to_sbatch.sh
@@ -28,5 +28,4 @@ snakemake \
     --retries 3 \
     --keep-going \
     --cores 64 \
-    --local-cores 2 \
-    output/030_iqtree/NUC.NT.muscle_align.gap0.2.cov0.8/iqtree_files.tar
+    --local-cores 2 
